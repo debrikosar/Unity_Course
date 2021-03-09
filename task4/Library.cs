1ea41d53aa945673d427ab41b7b44369e62cc5ad
debrikosar <debrikosar@gmail.com>
2021-03-09 09:44:32 +0300

    Syntax fix

--- task4/Library.cs
+++ task4/Library.cs
@@ -22,91 +22,93 @@ namespace Lection4
 {
     class Library
     {
-		private List<Book> libraryBooks;
+	    private List<Book> libraryBooks;
+	    
+	    public Library() : this(new List<Book>())
+	    {
 
-		public Library() : this(new List<Book>())
-        {
+	    }
+	    
+	    public Library(List<Book> books)
+	    {
+		    libraryBooks = books;
+	    }
 
-        }
+	    public List<Book> LibraryBooks
+	    {
+		    get
+		    {
+			    return libraryBooks;
+		    }
 
-		public Library(List<Book> books)
-        {
-			libraryBooks = books;
-		}
+		    set
+		    {
+			    libraryBooks = value;
+		    }
+	    }
+	    
+	    public void AddBook(string bookName)
+	    {
+		    libraryBooks.Add(new Book(bookName));
+	    }
+	    
+	    public void AddBook(Book book)
+	    {
+		    libraryBooks.Add(book);
+	    }
+	    
+	    public void RemoveBook(string bookName)
+	    {
+		    libraryBooks.RemoveAll(book => book.BookName == bookName);
+	    }
 
-		public List<Book> LibraryBooks
-        {
-			get
-            {
-				return libraryBooks;
-            }
+	    public void RemoveBook(Book book)
+	    {
+		    libraryBooks.Remove(book);
+	    }
 
-            set
-            {
-				libraryBooks = value;
-            }
-        }
+	    public void ShowBooks()
+	    {
+		    Console.WriteLine("\nCurrent library books: ");
 
-		public void AddBook(string bookName)
-        {
-			libraryBooks.Add(new Book(bookName));
-        }
+		    foreach(var book in libraryBooks)
+		    {
+			    Console.WriteLine(book.BookName);
+		    }
+	    }
 
-		public void AddBook(Book book)
-		{
-			libraryBooks.Add(book);
-		}
+	    public bool IsBookExist(string bookName)
+	    {
+		    return libraryBooks.Any(book => book.BookName == bookName);
+	    }
 
-		public void RemoveBook(string bookName)
-		{
-			libraryBooks.RemoveAll(book => book.BookName == bookName);
-		}
+	    public bool IsBookExist(Book book)
+	    {
+		    return libraryBooks.Contains(book);
+	    }
+    }
 
-		public void RemoveBook(Book book)
-		{
-			libraryBooks.Remove(book);
-		}
-
-		public void ShowBooks()
-        {
-			Console.WriteLine("\nCurrent library books: ");
-			foreach(var book in libraryBooks)
-            {
-				Console.WriteLine(book.BookName);
-            }
-        }
-
-		public bool IsBookExist(string bookName)
-        {
-			return libraryBooks.Any(book => book.BookName == bookName);
-		}
-
-		public bool IsBookExist(Book book)
-		{
-			return libraryBooks.Contains(book);
-		}
-	}
-
-	struct Book
+    struct Book
     {
-		private string bookName;
+	    private string bookName;
 
-		public Book(string bookName)
-        {
-			this.bookName = bookName;
-        }
 
-		public string BookName
-        {
-            get
-            {
-				return bookName;
-            }
+	    public Book(string bookName)
+	    {
+		    this.bookName = bookName;
+	    }
 
-            set
-            {
-				bookName = value;
-            }
-        }
-	}
+	    public string BookName
+	    {
+		    get
+		    {
+			    return bookName;
+		    }
+		    
+		    set
+		    {
+			    bookName = value;
+		    }
+	    }
+    }
 }
